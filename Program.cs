c692c5999ee0ed786e2b965aa10cf1501fb4b6fb
cavalenciad <cavalenciad@outlook.com>
2025-06-27 19:16:36 -0500

    Add CORS policy to allow requests from RemixApp

--- Program.cs
+++ Program.cs
@@ -34,6 +34,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowRemixApp",
+        policy =>
+            {
+                policy.WithOrigins("http://localhost:5173")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+    });
+});
+
 // --- Fin de Servicios ---
 
 var app = builder.Build();
